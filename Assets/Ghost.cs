e2cb433f66cf6675ffbf2d296346993ec567cd91
elan_rubin <elandanielrubin@gmail.com>
2023-10-20 18:30:57 -0400

    ghost catching

--- Assets/Ghost.cs
+++ Assets/Ghost.cs
@@ -1,13 +1,17 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ghost : MonoBehaviour
 {
-    private Vector2 currentPosition, targetPosition;
-    float timer = 0f;
-    float timer2 = 0f;
+    private Vector2 currentPosition, targetPosition, prevTargetPosition;
+    float timer, timer2, timer3;
+    Image circle;
+    TextMeshProUGUI text;
     bool caught;
 
 
@@ -15,37 +19,66 @@ public class Ghost : MonoBehaviour
     {
         currentPosition = targetPosition = transform.position;
         timer2 = Random.Range(0, 1);
+        circle = transform.GetChild(1).GetChild(0).GetComponent<Image>();
+        text = transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>();
     }
 
     void Update()
     {
-        if (caught) return;
         timer -= Time.deltaTime;
         timer2 += Time.deltaTime / 3f;
-        if (timer < 0)
+        if (timer < 0 && !caught)
         {
             //var dif = PlayerMovement.Instance.PlayerPosition - currentPosition;
-            targetPosition = Vector2.MoveTowards(currentPosition,PlayerMovement.Instance.PlayerPosition,-1);
+            targetPosition = Vector2.MoveTowards(currentPosition, PlayerMovement.Instance.PlayerPosition, -1);
             targetPosition += new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)) / 2f;
             transform.GetChild(0).GetComponent<SpriteRenderer>().flipX = targetPosition.x < currentPosition.x;
-            timer = Random.Range(1f,3f);
+            timer = Random.Range(1f, 3f);
         }
 
-        transform.position = currentPosition = Vector2.Lerp(currentPosition, targetPosition, Time.deltaTime * 3f);
-        transform.GetChild(0).transform.localPosition = Vector2.up * Mathf.Sin(timer2 * 360f * Mathf.Deg2Rad);
-        if (Input.GetMouseButtonDown(0) && Vector2.Distance(transform.position, CameraManager.Instance.LaggedMousePos) < 1f) StartCoroutine(nameof(Caught));
+        text.gameObject.SetActive(caught);
+        if (!caught) transform.position = currentPosition = Vector2.Lerp(currentPosition, targetPosition, Time.deltaTime * 3f);
+        if (!caught) transform.GetChild(0).transform.localPosition = Vector2.up * Mathf.Sin(timer2 * 360f * Mathf.Deg2Rad);
+        if (!caught&&timer3>0) timer3 -= Time.deltaTime;
+        if (Vector2.Distance(transform.position, CameraManager.Instance.LaggedMousePos) < 1f)
+        {
+            if (caught)
+            {
+                if (timer3 < 1.5f) timer3 += Time.deltaTime;
+                circle.fillAmount = timer3 / 1.5f;
+                text.text = $"{(int)(timer3 / 1.5f * 100)}";
+
+                //targetPosition = PlayerMovement.Instance.PlayerPosition;
+            }
+            else
+            {
+                prevTargetPosition = targetPosition;
+            }
+            caught = true;
+        }
+        else if (caught)
+        {
+            caught = false;
+            targetPosition = prevTargetPosition;
+        }
     }
     private void OnMouseOver()
     {
-        if (Input.GetMouseButtonDown(0))
+        caught = true;
+        /*if (Input.GetMouseButtonDown(0))
         {
             StartCoroutine(nameof(Caught));
-        }
+        }*/
     }
-    private IEnumerator Caught()
+    private void OnMouseExit()
+    {
+        caught = false;
+        targetPosition = prevTargetPosition;
+    }
+    /*private IEnumerator Caught()
     {
         caught = true;
         transform.DOMove(PlayerMovement.Instance.PlayerPosition, 1f);
         yield return null;
-    }
+    }*/
 }
